75e29299187caa4e0583129966ca90a0e972677a
RizalwanAR <rizalwanardi@gmail.com>
2022-04-01 22:33:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function insert(num) {
+    document.form.textView.value = document.form.textView.value + num;
+}
+function c() {
+    document.form.textView.value = "";
+}
+function del() {
+    var x = document.form.textView.value;
+    document.form.textView.value = x.substring(0, x.length - 1)
+}
+function equal() {
+    var x = document.form.textView.value;
+    if (x == "") {
+        alert("anda harus memasukkan angka dahulu.")
+    }
+    if (x) {
+        document.form.textView.value = eval(x);
+    }
+
+}
